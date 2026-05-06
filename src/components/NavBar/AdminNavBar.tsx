17b419eda935028353c42d11a7c96d15131b933e
AndreeaJigarov <andreea.jigarov@stud.ubbcluj.ro>
2026-05-07 00:53:37 +0300

    commit

--- src/components/NavBar/AdminNavBar.tsx
+++ src/components/NavBar/AdminNavBar.tsx
@@ -19,15 +19,15 @@ export const AdminNavbar = () => {
             <img src={logo_White} alt="Equitas Logo" className={styles.logoImage} />
 
             <div className={styles.nav}>
-                <NavLink
-                    to="/"
-                    end
-                    className={({ isActive }) =>
-                        `${styles.navLink} ${isActive ? styles.active : ''}`
-                    }
-                >
-                    Home
-                </NavLink>
+                {/*<NavLink*/}
+                {/*    to="/"*/}
+                {/*    end*/}
+                {/*    className={({ isActive }) =>*/}
+                {/*        `${styles.navLink} ${isActive ? styles.active : ''}`*/}
+                {/*    }*/}
+                {/*>*/}
+                {/*    Home*/}
+                {/*</NavLink>*/}
                 <NavLink
                     to="/horses"
                     className={({ isActive }) =>
